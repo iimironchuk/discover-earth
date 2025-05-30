b4fce8d0963df5290a7254d480c8b85eb74a6442
iimironchuk <iramyronchuk26@gmail.com>
2025-05-30 14:27:01 +0300

    main page

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,22 +1,103 @@
 import 'package:discover_earth/resources/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:responsive_framework/responsive_framework.dart';
+
+import '../gen/assets.gen.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(68.0),
-        child: AppBar(
-          backgroundColor: AppColors.scaffold,
-          title: Row(children: [
-            Text('Eden Reverie', style: Theme.of(context).textTheme.titleMedium,),
-
-          ],),
+    final smallerThanDesktop = ResponsiveBreakpoints.of(
+      context,
+    ).smallerThan(DESKTOP);
+    final smallerThanTablet = ResponsiveBreakpoints.of(
+      context,
+    ).smallerThan(TABLET);
+    return Stack(
+      children: [
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height - 68.0,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: Assets.images.mainImage.provider(),
+              fit: BoxFit.cover,
+            ),
+          ),
         ),
-      ),
+        Positioned(
+          left: MediaQuery.of(context).size.width * 1 / 8,
+          top: MediaQuery.of(context).size.height * 1 / 5,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Discover Earth`s Most\nSacred Sanctuaries',
+                style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                  fontSize:
+                      smallerThanTablet
+                          ? 45.0
+                          : smallerThanDesktop
+                          ? 50.0
+                          : 72.0,
+                  color: AppColors.scaffold,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                  top: 26.0,
+                  bottom: 42.0,
+                  right: MediaQuery.of(context).size.width * 1 / 8,
+                ),
+                child: SizedBox(
+                  width: 500.0,
+                  child: Text(
+                    'An immersive journey into the world`s most untouched natural wonders, curated for those who seek the extraordinary.',
+                    style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                      fontSize: smallerThanDesktop
+                          ? 16.0
+                          : 20.0,
+                      fontWeight: FontWeight.w300,
+                      color: AppColors.scaffold,
+                    ),
+                    softWrap: true,
+                    overflow: TextOverflow.visible,
+                  ),
+                ),
+              ),
+              Row(
+                children: [
+                  SizedBox(
+                    width: 200.0,
+                    height: 44.0,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text('Begin Your Journey'),
+                    ),
+                  ),
+                  SizedBox(width: 24.0),
+                  Switch(
+                    value: true,
+                    onChanged: (isOn) {},
+                    activeColor: AppColors.scaffold,
+                    activeTrackColor: AppColors.mainSand,
+                  ),
+                  SizedBox(width: 12.0),
+                  Text(
+                    'Ambient Sound',
+                    style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                      color: AppColors.scaffold,
+                      fontSize: 14.0,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
